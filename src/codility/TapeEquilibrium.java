240c19322793a671cf800b38bc4fa24575e1ca9f
michael <michaelsantunes@gmail.com>
2019-06-29 21:50:30 +0100

    TapeEquilibrium

--- src/codility/TapeEquilibrium.java
+++ src/codility/TapeEquilibrium.java
@@ -1,22 +1,73 @@
 package codility;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 public class TapeEquilibrium {
 
+/*
+A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+
+Any integer P, such that 0 < P < N, splits this tape into two non-empty parts:
+A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+
+The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+
+In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+
+For example, consider array A such that:
+
+A[0] = 3
+A[1] = 1
+A[2] = 2
+A[3] = 4
+A[4] = 3
+We can split this tape in four places:
+
+P = 1, difference = |3 − 10| = 7
+P = 2, difference = |4 − 9| = 5
+P = 3, difference = |6 − 7| = 1
+P = 4, difference = |10 − 3| = 7
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
+
+For example, given:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+the function should return 1, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [2..100,000];
+each element of array A is an integer within the range [−1,000..1,000].
+
+*/
+
     public static int solution( int[] A) {
-        int p = Integer.MAX_VALUE;
-        int sum = Arrays.stream(A).sum();
-        int left = 0;
-        for ( int i = 0; i < A.length - 1; i++) {
+        int min   = Integer.MAX_VALUE;
+        int sum   = Arrays.stream(A).sum();
+        int left  = 0;
+        int right = 0;
+        for ( int i = 0; i < A.length-1; i++ ) {
             left += A[i];
-            int right = sum - left;
-            int diff = Math.abs(left - right);
-            if ( diff < p ) {
-                p = diff;
+            right = sum - left;
+            int diff = Math.abs(right - left);
+            if ( diff < min ) {
+                min = diff;
             }
         }
-        return p;
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {9,3,1,3,4,9,10,293,12,38,110,802,10000,2,3,13,28390,1002};
+        System.out.println(solution(A));
     }
 }
