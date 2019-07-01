7cd7a8e8e4328b7a8dc35c268c00f3d53731f3f6
michael <michaelsantunes@gmail.com>
2019-07-01 03:09:09 +0100

    countingValleys

---
+++ src/codility/Niobium.java
@@ -0,0 +1,13 @@
+package codility;
+
+public class Niobium {
+
+
+    public static int solution() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/hackerrank/CountingValleys.java
@@ -0,0 +1,30 @@
+package hackerrank;
+
+public class CountingValleys {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+        int up = 0;
+        int down = 0;
+        int valley = 0;
+        String[] array = s.split("");
+
+        for ( int i = 0; i < n; i++ ) {
+            if ( array[i].equalsIgnoreCase("U") ) {
+                up++;
+            } else {
+                down++;
+            }
+            if ( (up - down ) == 0 && array[i].equalsIgnoreCase("U")) {
+                valley++;
+            }
+        }
+        return valley;
+
+    }
+
+    public static void main(String[] args) {
+        String sequence = "DDUUDDUDUUUD";
+        System.out.println(countingValleys(sequence.length(), sequence));
+    }
+}
